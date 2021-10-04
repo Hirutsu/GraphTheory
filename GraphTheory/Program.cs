6b1550286621d1e138dd114684c0e890e1aa9b53
Hirutsu <official_hirutsu@mail.ru>
2021-10-04 18:34:18 +0400

    beta interface

--- GraphTheory/Program.cs
+++ GraphTheory/Program.cs
@@ -22,9 +22,20 @@ namespace GraphTheory
             }
             if (num == 2)
             {
-                Console.Write("Введите путь к файлу: ");
+                Console.WriteLine("Выберите файл: ");
+                Console.WriteLine("1.Ориентированный взвешенный: ");
+                Console.WriteLine("2.Ориентированный невзвешенный: ");
+                Console.WriteLine("3.Неориентированный взвешенный: ");
+                Console.WriteLine("4.Неориетированный невзвешенный: ");
                 string filePath = Console.ReadLine();
-                graph = new Graph(@"C:\Users\burchuladzela\Desktop\GraphTheory\GraphTheory\InPutGraph.txt");
+                if (filePath == "1")
+                    graph = new Graph("../GraphTheory/GraphTheory/OriWeiGraph.txt");
+                else if (filePath == "2")
+                    graph = new Graph("../GraphTheory/GraphTheory/OriNoWeiGraph.txt");
+                else if (filePath == "3")
+                    graph = new Graph("../GraphTheory/GraphTheory/NoOriWeiGraph.txt");
+                else
+                    graph = new Graph("../GraphTheory/GraphTheory/NoOriNoWeiGraph.txt");
             }
             while(num != 7)
             {
@@ -39,21 +50,28 @@ namespace GraphTheory
                 Console.Write("Введите число:");
                 num = Convert.ToInt32(Console.ReadLine());
                 string str;
+                string[] strArr;
                 switch (num)
                 {
                     case 1:
-                        Console.Write("Введите название вершнины: ");
+                        Console.Write("Введите название вершнины,которую нужно добавить: ");
                         str = Console.ReadLine();
                         graph.AddVertex(str);
                         break;
                     case 2:
-                        Console.Write("Введите название вершины: ");
+                        Console.Write("Введите название вершины,которую нужно удалить: ");
                         str = Console.ReadLine();
                         graph.DeleteVertex(str);
                         break;
                     case 3:
+                        Console.Write("Введите ('из', 'куда', 'вес') для добавления ребра: ");
+                        strArr = Console.ReadLine().Split();
+                        graph.AddEdge(strArr[0], strArr[1], Convert.ToInt32(strArr[2]));
                         break;
                     case 4:
+                        Console.Write("Введите ('из', 'куда') для удаления ребра: ");
+                        strArr = Console.ReadLine().Split();
+                        graph.DeleteEdge(strArr[0], strArr[1]);
                         break;
                     case 5:
                         graph.ShowToConsole();
